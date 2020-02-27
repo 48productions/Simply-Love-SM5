51f77aeec5f3211fab99d8c58fba44d8ecede3c3
48productions <57228987+48productions@users.noreply.github.com>
2020-02-27 12:24:17 -0700

    ScreenMemoryCard displays alt text if memory cards are disabled or if custom songs are enabled

--- BGAnimations/ScreenMemoryCard overlay/default.lua
+++ BGAnimations/ScreenMemoryCard overlay/default.lua
@@ -1,20 +1,39 @@
-return Def.ActorFrame{
-	InitCommand=function(self) self:Center() end,
+local TopString = ScreenString("Top")  --Default the first string to an alternate prompt for when memory cards arne't enabled
+local BottomerString = ""
 
-	LoadActor("usbicon.png")..{
-		InitCommand=function(self) self:shadowlength(1) end,
-		OnCommand=function(self) self:zoom(0.6):glow(1,1,1,1):glowshift():diffusealpha(0):sleep(1):decelerate(2):diffusealpha(1):sleep(6):linear(0.75):diffusealpha(0) end,
-	},
-
-	LoadFont("Common Normal")..{
-		Text=ScreenString("Top"),
-		InitCommand=function(self) self:shadowlength(1):y(-60):diffusealpha(0) end,
-		OnCommand=function(self) self:sleep(2.0):decelerate(1):diffusealpha(1):sleep(6):linear(0.75):diffusealpha(0) end,
-	},
-
-	LoadFont("Common Normal")..{
-		Text=ScreenString("Bottom"),
-		InitCommand=function(self) self:shadowlength(1):y(60):diffusealpha(0) end,
-		OnCommand=function(self) self:sleep(3.0):decelerate(1):diffusealpha(1):sleep(5):linear(0.75):diffusealpha(0) end,
-	}
+local af = Def.ActorFrame{
+	InitCommand=function(self) self:Center() end
 }
+
+--SCREENMAN:SystemMessage(PREFSMAN:GetPreference("MemoryCards"))
+if PREFSMAN:GetPreference("MemoryCards") ~= true then --Memory cards aren't enabled, display alternate prompts
+	TopString = ScreenString("TopNoMC")
+	BottomerString = ScreenString("LocalProfileInfo")
+elseif PREFSMAN:GetPreference("CustomSongsEnable") then --Memory cards are enabled and custom songs are enabled, display an extra prompt
+	BottomerString = ScreenString("CustomSongs")
+end
+
+af[#af+1] = LoadActor("usbicon.png")..{ --USB drive icon
+	InitCommand=function(self) self:shadowlength(1) end,
+	OnCommand=function(self) self:zoom(0.6):glow(1,1,1,1):glowshift():diffusealpha(0):sleep(1):decelerate(2):diffusealpha(1):sleep(6):linear(0.75):diffusealpha(0) end,
+}
+
+af[#af+1] = LoadFont("Common Normal")..{ --Top text (create a profile or a use a USB drive)
+	Text=TopString,
+	InitCommand=function(self) self:shadowlength(1):y(-60):diffusealpha(0) end,
+	OnCommand=function(self) self:sleep(2.0):decelerate(1):diffusealpha(1):sleep(6):linear(0.75):diffusealpha(0) end,
+}
+
+af[#af+1] = LoadFont("Common Normal")..{ --Bottom text (...to save scores/prefs)
+	Text=ScreenString("Bottom"),
+	InitCommand=function(self) self:shadowlength(1):y(60):diffusealpha(0) end,
+	OnCommand=function(self) self:sleep(3.0):decelerate(1):diffusealpha(1):sleep(5):linear(0.75):diffusealpha(0) end,
+}
+
+af[#af+1] = LoadFont("Common Normal")..{ --Bottomer text (local profile/custom songs)
+	Text=BottomerString,
+	InitCommand=function(self) self:shadowlength(1):y(90):diffusealpha(0) end,
+	OnCommand=function(self) self:sleep(4.0):decelerate(1):diffusealpha(1):sleep(4):linear(0.75):diffusealpha(0) end,
+}
+
+return af
