e059b4e4b77acb5f6b3ec7eb21cf85f2a124ac75
tiktalik <mrtiktalik@gmail.com>
2020-02-29 11:51:04 -0700

    More Upheaval.

--- Graphics/ScreenSelectPlayMode Icon.lua
+++ Graphics/ScreenSelectPlayMode Icon.lua
@@ -3,12 +3,12 @@ local index = gc:GetIndex()
 local text = gc:GetName()
 
 -- text description of each mode ("Casual", "ITG", "FA+", "StomperZ")
-return LoadFont("_wendy small")..{
+return LoadFont("_upheaval_underline 80px")..{
 	Name="ModeName"..index,
 	Text=ScreenString(text),
 
-	InitCommand=function(self) self:halign(1):maxwidth(256) end,
-	GainFocusCommand=function(self) self:stoptweening():linear(0.1):zoom(0.75):diffuse(PlayerColor(PLAYER_1)) end,
+	InitCommand=function(self) self:halign(0.95):valign(0.65)maxwidth(256) end,
+	GainFocusCommand=function(self) self:stoptweening():linear(0.1):zoom(0.5):diffuse(PlayerColor(PLAYER_1)) end,
 	LoseFocusCommand=function(self) self:stoptweening():linear(0.1):zoom(0.3):diffuse(color("#888888")) end,
 	OffCommand=function(self) self:linear(0.2):diffusealpha(0) end
 }
