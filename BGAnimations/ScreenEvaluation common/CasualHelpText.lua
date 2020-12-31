aed9e10d42c89abe06b442a1a6dbec2f70e3b232
48productions <48productions@gmail.com>
2020-12-31 15:42:13 -0700

    Upheavalification Pt. 4
     - The game over text now uses upheaval (and is now also pre-rendered, because font zooming is fun)
     - The eval screen's casual mode help text also now uses upheaval

--- BGAnimations/ScreenEvaluation common/CasualHelpText.lua
+++ BGAnimations/ScreenEvaluation common/CasualHelpText.lua
@@ -1,15 +1,15 @@
 if SL.Global.GameMode ~= "Casual" then return end
 
 return Def.BitmapText{
-	Font="_wendy small",
+	Font="_upheaval_underline 80px",
 	Text=THEME:GetString("ScreenEvaluation", "PressStartToContinue"),
 	InitCommand=function(self)
-		self:xy(_screen.cx, _screen.cy + 170):zoom(0.55)
+		self:xy(_screen.cx, _screen.cy + 170):zoom(0.35)
 			:diffusealpha(0):shadowlength(0.5)
 	end,
 	OnCommand=function(self)
-		self:sleep(3):diffusealpha(1)
+		self:sleep(2):diffusealpha(1)
 			:diffuseshift():effectperiod(3)
-			:effectcolor1(1,1,1,0):effectcolor2(1,1,1,1)
+			:effectcolor1(1,1,1,1):effectcolor2(1,1,1,0.25)
 	end
 }
