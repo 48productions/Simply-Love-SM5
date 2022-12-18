452e020efc9f328bff5d60cf2fc0fe73d30f07c2
48productions <48productions@gmail.com>
2022-12-18 14:08:18 -0700

    Remove GameOver Stats, Make Sort Label Look Good

--- BGAnimations/ScreenGameOver overlay/default.lua
+++ BGAnimations/ScreenGameOver overlay/default.lua
@@ -9,22 +9,6 @@ local logo_tween_offset = 0.13
 local t = Def.ActorFrame{
     OnCommand=function(self) self:propagate(true):queuecommand("AnimateLogo") end,
     
-	--Player 1 Stats BG
-	Def.Quad{
-		InitCommand=function(self)
-			self:zoomto(160,_screen.h):xy(80, _screen.h/2):diffuse(color("#00000099"))
-			if ThemePrefs.Get("RainbowMode") then self:diffuse(color("#000000dd")) end
-		end,
-	},
-
-	--Player 2 Stats BG
-	Def.Quad{
-		InitCommand=function(self)
-			self:zoomto(160,_screen.h):xy(_screen.w-80, _screen.h/2):diffuse(color("#00000099"))
-			if ThemePrefs.Get("RainbowMode") then self:diffuse(color("#000000dd")) end
-		end,
-	},
-    
     --"Thanks for playing"/USB reminder text
     LoadFont("_upheaval_underline 80px")..{
 		Text=ScreenString("ThanksForPlaying"),
@@ -42,62 +26,6 @@ local t = Def.ActorFrame{
 		PulseCommand=function(self) self:smooth(1.5):zoom(0.35):smooth(1.5):zoom(0.34):queuecommand("Pulse") end,
 	},
 }
-
-for player in ivalues(Players) do
-
-	local line_height = 28
-	local middle_line_y = 220
-	local x_pos = player == PLAYER_1 and 80 or _screen.w-80
-	local PlayerStatsAF = Def.ActorFrame{ Name="PlayerStatsAF_"..ToEnumShortString(player) }
-	local stats
-
-	-- first, check if this player is using a profile (local or MemoryCard)
-	if PROFILEMAN:IsPersistentProfile(player) then
-
-		-- if a profile is in use, grab gameplay stats for this session that are pertinent
-		-- to this specific player's profile (highscore name, calories burned, total songs played)
-		stats = LoadActor("PlayerStatsWithProfile.lua", player)
-
-		-- loop through those stats, adding them to the ActorFrame for this player as BitmapText actors
-		for i,stat in ipairs(stats) do
-			PlayerStatsAF[#PlayerStatsAF+1] = LoadFont("Common Normal")..{
-				Text=stat,
-				InitCommand=function(self)
-					self:diffuse(PlayerColor(player))
-						:xy(x_pos, (line_height*(i-1)) + 40)
-						:maxwidth(150)
-				end
-			}
-		end
-
-	end
-
-	-- draw a thin line (really just a Def.Quad) separating
-	-- the upper (profile) stats from the lower (general) stats
-	PlayerStatsAF[#PlayerStatsAF+1] = Def.Quad{
-		InitCommand=function(self)
-			self:zoomto(120,1):xy(x_pos, middle_line_y)
-				:diffuse( PlayerColor(player) )
-		end
-	}
-
-	-- retrieve general gameplay session stats for which a profile is not needed
-	stats = LoadActor("PlayerStatsWithoutProfile.lua", player)
-
-	-- loop through those stats, adding them to the ActorFrame for this player as BitmapText actors
-	for i,stat in ipairs(stats) do
-		PlayerStatsAF[#PlayerStatsAF+1] = LoadFont("Common Normal")..{
-			Text=stat,
-			InitCommand=function(self)
-				self:diffuse(PlayerColor(player))
-					:xy(x_pos, _screen.h - (line_height*i))
-					:maxwidth(150)
-			end
-		}
-	end
-
-	t[#t+1] = PlayerStatsAF
-end
 
 --[[if using_mem_card then --If any player is using a memory card, remind them to take it with them!
 	t[#t+1] = LoadActor("usbicon.png")..{
--- BGAnimations/ScreenSelectMusic overlay/SortDisplay.lua
+++ BGAnimations/ScreenSelectMusic overlay/SortDisplay.lua
@@ -1,13 +1,15 @@
 return Def.ActorFrame{
-    InitCommand=function(self)
-        self:xy(_screen.w, 32)
-    end,
+    InitCommand=function(self) self:xy(_screen.w, 32) end,
+    OffCommand=function(self) self:smooth(0.1):diffusealpha(0) end,
     Def.Quad{
-        InitCommand=function(self) self:zoomto(100,20):vertalign(top):horizalign(right):diffuse(color_slate2):diffusealpha(0.85) end
+        InitCommand=function(self) self:zoomto(100,20):vertalign(top):horizalign(right):diffuse(color_slate2):diffusealpha(0.85):fadeleft(0.1) end
+    },
+    Def.Quad{
+        InitCommand=function(self) self:zoomto(100,1):vertalign(top):horizalign(right):y(19):diffusealpha(0.5):fadeleft(0.3) end
     },
     LoadFont("Common normal")..{
 		InitCommand=function(self)
-			self:horizalign(left):vertalign(top):xy(-96,3):zoomx(0.65):zoomy(0.7):queuecommand("Set")
+			self:horizalign(left):vertalign(top):xy(-90,3):zoomx(0.65):zoomy(0.7):queuecommand("Set")
 		end,
         SortMessageCommand=function(self, args)
             self:queuecommand("Set")
